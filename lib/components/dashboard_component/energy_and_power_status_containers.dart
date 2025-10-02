87f8aac15438ff970df6929734008f7bf66ca107
Isaac <tolulopesoliu2005@gmail.com>
2025-10-02 16:20:50 +0100

    Update energy and power status

--- lib/components/dashboard_component/energy_and_power_status_containers.dart
+++ lib/components/dashboard_component/energy_and_power_status_containers.dart
@@ -4,6 +4,8 @@ import 'package:smartmeter/utilities/spacer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import '../../service/fault_status_provider.dart';
 
 import '../../pages/dashboard.dart';
 
